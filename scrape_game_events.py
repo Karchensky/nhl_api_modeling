48a56f6e8d3224e66136b96bd06f522949ebb72d
Karchensky <bryankarchensky@gmail.com>
2023-04-18 21:07:12 -0400

    event scraper set up, added to ddl

--- scrape_game_events.py
+++ scrape_game_events.py
@@ -2,14 +2,6 @@ from functions.scraper_game_events import scraper_game_events
 from functions.db_list_fresh_game_ids import db_list_fresh_game_ids
 from functions.db_append import db_append
 
-scrape = scraper_game_events('2022010001')
-distinct_events = scrape['EVENT_TYPE'].unique()
-
-print(distinct_events)
-print(scrape)
-
-
-
-# Scrape all the game logs that we don't already have recorded in our table
-# for game_id in db_list_fresh_game_ids("STG_GAME_EVENTS"):
-#     db_append('STG_GAME_EVENTS', scraper_game_events(game_id))
+# # Scrape all the game logs that we don't already have recorded in our table
+for game_id in db_list_fresh_game_ids("STG_GAME_EVENTS"):
+    db_append('STG_GAME_EVENTS', scraper_game_events(game_id))
